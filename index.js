884bc4ee5c09a1c7d33e2e99b6bb62cdcaa5bbe1
Vladilen <mininvlad@gmail.com>
2020-02-18 12:09:38 +0300

    Finish

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+const fruits = [
+  {id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348'},
+  {id: 2, title: 'Апельсины', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg'},
+  {id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg'},
+]
 
-const modal = $.modal()
+/*
+* 1. Динамически на основе массива вывести список карточек
+* 2. Показать цену в модалке (и это должна быть 1 модалка)
+* 3. Модалка для удаления с 2мя кнопками
+* ---------
+* 4. На основе $.modal нужно сделать другой плагин $.confirm (Promise)
+* */
+
+
+const modal = $.modal({
+  title: 'Vladilen Modal',
+  closable: true,
+  content: `
+    <p>Lorem ipsum dolor sit.</p>
+    <p>Lorem ipsum dolor sit.</p>
+  `,
+  width: '400px',
+  footerButtons: [
+    {text: 'Ок', type: 'primary', handler() {
+      console.log('Primary btn clicked')
+      modal.close()
+    }},
+    {text: 'Cancel', type: 'danger', handler() {
+        console.log('Danger btn clicked')
+        modal.close()
+      }}
+  ]
+})
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,37 +1,56 @@
+Element.prototype.appendAfter = function(element) {
+  element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop() {}
+
+function _createModalFooter(buttons = []) {
+  if (buttons.length === 0) {
+    return document.createElement('div')
+  }
+
+  const wrap = document.createElement('div')
+  wrap.classList.add('modal-footer')
+
+  buttons.forEach(btn => {
+    const $btn = document.createElement('button')
+    $btn.textContent = btn.text
+    $btn.classList.add('btn')
+    $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+    $btn.onclick = btn.handler || noop
+
+    wrap.appendChild($btn)
+  })
+
+  return wrap
+}
+
+
 function _createModal(options) {
+  const DEFAULT_WIDTH = '600px'
   const modal = document.createElement('div')
   modal.classList.add('vmodal')
   modal.insertAdjacentHTML('afterbegin', `
-    <div class="modal-overlay">
-      <div class="modal-window">
+    <div class="modal-overlay" data-close="true">
+      <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
         <div class="modal-header">
-          <span class="modal-title">Modal title</span>
-          <span class="modal-close">&times;</span>
+          <span class="modal-title">${options.title || 'Окно'}</span>
+          ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
         </div>
-        <div class="modal-body">
-          <p>Lorem ipsum dolor sit.</p>
-          <p>Lorem ipsum dolor sit.</p>
-        </div>
-        <div class="modal-footer">
-          <button>Ok</button>
-          <button>Cancel</button>
+        <div class="modal-body" data-content>
+          ${options.content || ''}
         </div>
       </div>
     </div>
   `)
+  const footer = _createModalFooter(options.footerButtons)
+  footer.appendAfter(modal.querySelector('[data-content]'))
   document.body.appendChild(modal)
   return modal
 }
 
 /*
-* title: string
-* closable: boolean
-* content: string
-* width: string ('400px')
-* destroy(): void
-* Окно должно закрываться
 * --------------
-* setContent(html: string): void | PUBLIC
 * onClose(): void
 * onOpen(): void
 * beforeClose(): boolean
@@ -42,9 +61,13 @@ $.modal = function(options) {
   const ANIMATION_SPEED = 200
   const $modal = _createModal(options)
   let closing = false
+  let destroyed = false
 
-  return {
+  const modal = {
     open() {
+      if (destroyed) {
+        return console.log('Modal is destroyed')
+      }
       !closing && $modal.classList.add('open')
     },
     close() {
@@ -55,7 +78,25 @@ $.modal = function(options) {
         $modal.classList.remove('hide')
         closing = false
       }, ANIMATION_SPEED)
-    },
-    destroy() {}
+    }
   }
+
+  const listener = event => {
+    if (event.target.dataset.close) {
+      modal.close()
+    }
+  }
+
+  $modal.addEventListener('click', listener)
+
+  return Object.assign(modal, {
+    destroy() {
+      $modal.parentNode.removeChild($modal)
+      $modal.removeEventListener('click', listener)
+      destroyed = true
+    },
+    setContent(html) {
+      $modal.querySelector('[data-content]').innerHTML = html
+    }
+  })
 }
